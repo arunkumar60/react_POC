a74ab80d19adfafd176cd91498a604c1aca80131
arunkumar60 <arunkumar9692@gmail.com>
2017-04-07 07:41:04 +0530

    react poc

---
+++ reactApp/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header.jsx';
+import Content from './components/Content.jsx';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <Header/>
+            <Content />
+         </div>
+      );
+   }
+}
+
+export default App;
+ 
---
+++ reactApp/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import Header from './components/Header.jsx';
+import Content from './components/Content.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
